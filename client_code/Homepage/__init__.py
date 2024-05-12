efdb7107f318874384f7d44d7d84c8346743aa68
dgeo_is_back@hotmail.com <dgeo_is_back@hotmail.com>
2024-05-12 12:23:28 +0000

    Created Homepage.Module1 and Homepage; Edited settings and Controller; Deleted Form1.Module1 and Form1

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -1,11 +1,11 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import HomepageTemplate
 from anvil import *
 import anvil.server
 
 
-class Form1(Form1Template):
+class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
+    anvil.server.call('CallSampleXml')
     # Any code you write here will run before the form opens.
